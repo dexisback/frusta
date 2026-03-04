ab091c9759da3cc2bf39ee7a842844211c1166e6
Amaan <texamaan86@gmail.com>
2026-03-05 04:33:02 +0530

    add: app.ts

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import uploadRoutes from "./modules/uploads/uploads.routes.js"
+import express from "express";
+const app = express();
+
+app.use(express.json())
+app.use("/uploads", uploadRoutes)
+//todo: error middleware
